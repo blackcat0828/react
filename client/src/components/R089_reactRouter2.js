c6520967bf1681bc0ca27901524e04985915da06
Jeff Kim <watb88@gmail.com>
2021-10-27 16:59:58 +0900

    1027

---
+++ client/src/components/R089_reactRouter2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class R089_reactRouter2 extends Component {
+    render() {
+        return (
+            <>
+                <h1>path='/reaxtRouter2'</h1>
+                <h3>라우터2</h3>
+            </>
+        );
+    }
+}
+
+export default R089_reactRouter2;
--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
-import App from './App';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux'
-import reducers from './reducers';
+import App from './components/App';
 
 
-const CallMiddleware = store => nextMiddle => action => {
-  console.log('1. reducer 실행 전');
-  console.log('2. action.type : ' + action.type+', store str : ' + store.getState().data.str);
-  let result = nextMiddle(action);
-  console.log('3. reducer 실행 후');
-  console.log('4. action.type : ' + action.type+', store str : '+ store.getState().data.str);
-  return result;
-}
 
-const store = createStore(reducers, applyMiddleware(CallMiddleware));
-
-const listener = ()=>{
-  ReactDOM.render(
-    <Provider store={store}>
-      <App indexProp="react"/>
-    </Provider>,
+  ReactDOM.render((
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      ),
     document.getElementById('root')
   );
-};
-
-store.subscribe(listener);
-listener();
-
